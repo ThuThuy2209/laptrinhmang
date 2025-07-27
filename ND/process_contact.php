e31297f166d75929b488b71f570905a19c25b883
Nhu Quynh <quynhnn4642@ut.edu.vn>
2025-07-27 22:09:09 +0700

    Update process_contact.php

--- ND/process_contact.php
+++ ND/process_contact.php
@@ -2,12 +2,17 @@
 include '../db.php';
 session_start();
 
+if (!isset($_SESSION['id'])) {
+    die("Bạn chưa đăng nhập!");
+}
+
+$user_id = $_SESSION['id'];
 $name = $_POST['name'];
 $category = $_POST['category'];
 $message = $_POST['message'];
 
-$sql = "INSERT INTO feedback (name, category, message) 
-        VALUES ('$name', '$category', '$message')";
+$sql = "INSERT INTO feedback (user_id, name, category, message) 
+        VALUES ('$user_id', '$name', '$category', '$message')";
 
 if ($conn->query($sql) === TRUE) {
     echo "<script>alert('Phản hồi đã được gửi!'); window.location.href='index.php';</script>";
