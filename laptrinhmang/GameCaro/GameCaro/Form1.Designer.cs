f8844be273416c632e940c95d17328a7b5c42323
Chu Thu Thuy <thuyct5730@ut.edu.vn>
2025-08-01 14:07:38 +0700

    Add files via upload

---
+++ laptrinhmang/GameCaro/GameCaro/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace GameCaro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnlChessBoard = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.picbAvatar = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLan = new System.Windows.Forms.Button();
+            this.txbIP = new System.Windows.Forms.TextBox();
+            this.pctbMark = new System.Windows.Forms.PictureBox();
+            this.prcbCoolDown = new System.Windows.Forms.ProgressBar();
+            this.txbPlayerName = new System.Windows.Forms.TextBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picbAvatar)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbMark)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlChessBoard
+            // 
+            this.pnlChessBoard.BackColor = System.Drawing.SystemColors.Control;
+            this.pnlChessBoard.Location = new System.Drawing.Point(11, 10);
+            this.pnlChessBoard.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlChessBoard.Name = "pnlChessBoard";
+            this.pnlChessBoard.Size = new System.Drawing.Size(772, 593);
+            this.pnlChessBoard.TabIndex = 0;
+            this.pnlChessBoard.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlChessBoard_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.picbAvatar);
+            this.panel2.Location = new System.Drawing.Point(788, 10);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(298, 283);
+            this.panel2.TabIndex = 1;
+            // 
+            // picbAvatar
+            // 
+            this.picbAvatar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.picbAvatar.BackColor = System.Drawing.SystemColors.Control;
+            this.picbAvatar.BackgroundImage = global::GameCaro.Properties.Resources.OIP;
+            this.picbAvatar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picbAvatar.Location = new System.Drawing.Point(0, 0);
+            this.picbAvatar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.picbAvatar.Name = "picbAvatar";
+            this.picbAvatar.Size = new System.Drawing.Size(298, 283);
+            this.picbAvatar.TabIndex = 0;
+            this.picbAvatar.TabStop = false;
+            this.picbAvatar.Click += new System.EventHandler(this.picbAvatar_Click_1);
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.btnLan);
+            this.panel3.Controls.Add(this.txbIP);
+            this.panel3.Controls.Add(this.pctbMark);
+            this.panel3.Controls.Add(this.prcbCoolDown);
+            this.panel3.Controls.Add(this.txbPlayerName);
+            this.panel3.Location = new System.Drawing.Point(788, 298);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(298, 274);
+            this.panel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Elephant", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 35);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "5 in a line to win";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnLan
+            // 
+            this.btnLan.Location = new System.Drawing.Point(2, 125);
+            this.btnLan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnLan.Name = "btnLan";
+            this.btnLan.Size = new System.Drawing.Size(161, 24);
+            this.btnLan.TabIndex = 4;
+            this.btnLan.Text = "LAN";
+            this.btnLan.UseVisualStyleBackColor = true;
+            this.btnLan.Click += new System.EventHandler(this.btnLan_Click);
+            // 
+            // txbIP
+            // 
+            this.txbIP.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbIP.Location = new System.Drawing.Point(2, 99);
+            this.txbIP.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txbIP.Name = "txbIP";
+            this.txbIP.Size = new System.Drawing.Size(160, 22);
+            this.txbIP.TabIndex = 3;
+            this.txbIP.Text = "127.0.0.1";
+            this.txbIP.TextChanged += new System.EventHandler(this.txbIP_TextChanged);
+            // 
+            // pctbMark
+            // 
+            this.pctbMark.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pctbMark.Location = new System.Drawing.Point(167, 46);
+            this.pctbMark.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pctbMark.Name = "pctbMark";
+            this.pctbMark.Size = new System.Drawing.Size(128, 98);
+            this.pctbMark.TabIndex = 2;
+            this.pctbMark.TabStop = false;
+            // 
+            // prcbCoolDown
+            // 
+            this.prcbCoolDown.Location = new System.Drawing.Point(3, 72);
+            this.prcbCoolDown.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.prcbCoolDown.Name = "prcbCoolDown";
+            this.prcbCoolDown.Size = new System.Drawing.Size(160, 23);
+            this.prcbCoolDown.TabIndex = 1;
+            this.prcbCoolDown.Click += new System.EventHandler(this.prcbCoolDown_Click);
+            // 
+            // txbPlayerName
+            // 
+            this.txbPlayerName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbPlayerName.Location = new System.Drawing.Point(3, 46);
+            this.txbPlayerName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txbPlayerName.Name = "txbPlayerName";
+            this.txbPlayerName.ReadOnly = true;
+            this.txbPlayerName.Size = new System.Drawing.Size(160, 22);
+            this.txbPlayerName.TabIndex = 0;
+            this.txbPlayerName.TextChanged += new System.EventHandler(this.txbPlayerName_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 623);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnlChessBoard);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Game Caro LAN";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picbAvatar)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbMark)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlChessBoard;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox picbAvatar;
+        private System.Windows.Forms.TextBox txbPlayerName;
+        private System.Windows.Forms.Button btnLan;
+        private System.Windows.Forms.TextBox txbIP;
+        private System.Windows.Forms.PictureBox pctbMark;
+        private System.Windows.Forms.ProgressBar prcbCoolDown;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
