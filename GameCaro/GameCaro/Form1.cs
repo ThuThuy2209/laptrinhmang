988bca8c766d3d5257869ef451a6df6c4809e52f
MP-1611 <phuoctm7413@ut.edu.vn>
2025-08-01 13:46:12 +0700

    Add files via upload

---
+++ GameCaro/GameCaro/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameCaro
+{
+    public partial class Form1 : Form
+    {
+        #region Properties
+        ChessBoardManager ChessBoard;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+
+            ChessBoard = new ChessBoardManager(pnlChessBoard);
+
+            ChessBoard.DrawChessBoard();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void picbAvatar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLan_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void prcbCoolDown_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txbIP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picbAvatar_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txbPlayerName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlChessBoard_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
